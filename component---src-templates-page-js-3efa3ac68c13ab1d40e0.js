3442e4692b3dc754d38f4135057a993b3bbbda32
shan5742 <asamshan456@gmail.com>
2020-09-04 10:29:38 +0100

    Updates

---
+++ component---src-templates-page-js-3efa3ac68c13ab1d40e0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{sweJ:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return u}));var a=n("q1tI"),r=n.n(a),l=n("Wbzz"),u="2802998714";t.default=function(e){var t=e.data,n=(e.errors,t&&t.mockDataCsv);return r.a.createElement("div",null,r.a.createElement("h1",null,n.title),r.a.createElement("h3",null,n.subtitle),r.a.createElement("p",null,n.content),r.a.createElement("button",{onClick:function(){return Object(l.b)("/")}},"Home"))}}}]);
+//# sourceMappingURL=component---src-templates-page-js-3efa3ac68c13ab1d40e0.js.map
