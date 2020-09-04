6ee3db6c901eb4f7045beaf407934e9d21bc7a93
shan5742 <asamshan456@gmail.com>
2020-09-04 10:24:34 +0100

    Updates

---
+++ component---src-templates-page-js-a923a5cf10d2d724e453.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{sweJ:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return l}));var a=n("q1tI"),r=n.n(a),l="2802998714";t.default=function(e){var t=e.data,n=(e.errors,t&&t.mockDataCsv);return r.a.createElement("div",null,r.a.createElement("h1",null,n.title),r.a.createElement("h3",null,n.subtitle),r.a.createElement("p",null,n.content))}}}]);
+//# sourceMappingURL=component---src-templates-page-js-a923a5cf10d2d724e453.js.map
